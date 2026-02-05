c10c147759592d4c7100f8df1e2489b39be350cf
selendic <markselendic@gmail.com>
2026-02-05 13:45:41 +0100

    Added mandelbrot skeleton.

---
+++ mandelbrot.py
@@ -0,0 +1,22 @@
+"""
+Mandelbrot Set Generator
+Author : Marko Šelendić
+Course : Numerical Scientific Computing 2026
+"""
+
+def f(x):
+    """
+    Example function.
+
+    Parameters
+    ----------
+    x : float
+        Input value
+
+    Returns
+    -------
+    float
+        Output value
+    """
+    # TODO: Implement the algorithm
+    pass
